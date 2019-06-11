fecabb07b93200320d0b44d8f0c5b98b6ddf5405
Lucas <lucas@Ashleys-MacBook-Pro.local>
2019-06-11 01:09:48 -0600

    Updated VanillaJS with Black Diamond

--- Part1/index.js
+++ Part1/index.js
@@ -1,20 +1,28 @@
-console.log ('Hello world')
-
 let progress = []
 let board = []
+
+function resetGame () {
+    board = []
+    progress = []
+    for (let i=0; i<= 8; i++) {
+        let reset = document.getElementById(i)
+        reset.innerText = ''
+    }
+    document.getElementById('player').innerText = 'X'
+}
 
 function play(clickedId) {
     const playerSpan = document.getElementById('player')
     const choice = document.getElementById(clickedId)
     const check = document.getElementsByTagName('td')
 
-    if (playerSpan.innerText === 'X') {
+    if (playerSpan.innerText === 'X' && choice.innerText === '') {
         choice.innerText = 'X'
         progress.splice(clickedId, 0, 'X')
         playerSpan.innerText = 'O'
         board.splice(clickedId,0,'X')
         
-    } else {
+    } else if (choice.innerText === '') {
         choice.innerText = 'O'
         progress.splice(clickedId, 0, 'O')
         playerSpan.innerText = 'X'
@@ -23,38 +31,55 @@ function play(clickedId) {
     }
 
     if (check[0].innerText === 'X' && check[1].innerText === 'X' && check[2].innerText === 'X') {
-        return alert ('Player X Wins!')
+        alert ('Player X Wins!')
+        resetGame ()
     } else if (check[3].innerText === 'X' && check[4].innerText === 'X' && check[5].innerText === 'X') {
-        return alert ('Player X Wins!')
+        alert ('Player X Wins!')
+        resetGame ()
     } else if (check[6].innerText === 'X' && check[7].innerText === 'X' && check[8].innerText === 'X') {
-        return alert ('Player X Wins!')
+        alert ('Player X Wins!')
+        resetGame ()
     } else if (check[0].innerText === 'X' && check[3].innerText === 'X' && check[6].innerText === 'X') {
-        return alert ('Player X Wins!')
+        alert ('Player X Wins!')
+        resetGame ()
     } else if (check[1].innerText === 'X' && check[4].innerText === 'X' && check[7].innerText === 'X') {
-        return alert ('Player X Wins!')
+        alert ('Player X Wins!')
+        resetGame ()
     } else if (check[2].innerText === 'X' && check[5].innerText === 'X' && check[8].innerText === 'X') {
-        return alert ('Player X Wins!')
+        alert ('Player X Wins!')
+        resetGame ()
     } else if (check[0].innerText === 'X' && check[4].innerText === 'X' && check[8].innerText === 'X') {
-        return alert ('Player X Wins!')
+        alert ('Player X Wins!')
+        resetGame ()
     } else if (check[6].innerText === 'X' && check[4].innerText === 'X' && check[2].innerText === 'X') {
-        return alert ('Player X Wins!')
+        alert ('Player X Wins!')
+        resetGame ()
     } else if (check[0].innerText === 'O' && check[1].innerText === 'O' && check[2].innerText === 'O') {
-        return alert ('Player O Wins!')
+        alert ('Player O Wins!')
+        resetGame ()
     } else if (check[3].innerText === 'O' && check[4].innerText === 'O' && check[5].innerText === 'O') {
-        return alert ('Player O Wins!')
+        alert ('Player O Wins!')
+        resetGame ()
     } else if (check[6].innerText === 'O' && check[7].innerText === 'O' && check[8].innerText === 'O') {
-        return alert ('Player O Wins!')
+        alert ('Player O Wins!')
+        resetGame ()
     } else if (check[0].innerText === 'O' && check[3].innerText === 'O' && check[6].innerText === 'O') {
-        return alert ('Player O Wins!')
+        alert ('Player O Wins!')
+        resetGame ()
     } else if (check[1].innerText === 'O' && check[4].innerText === 'O' && check[7].innerText === 'O') {
-        return alert ('Player O Wins!')
+        alert ('Player O Wins!')
+        resetGame ()
     } else if (check[2].innerText === 'O' && check[5].innerText === 'O' && check[8].innerText === 'O') {
-        return alert ('Player O Wins!')
+        alert ('Player O Wins!')
+        resetGame ()
     } else if (check[0].innerText === 'O' && check[4].innerText === 'O' && check[8].innerText === 'O') {
-        return alert ('Player O Wins!')
+        alert ('Player O Wins!')
+        resetGame ()
     } else if (check[6].innerText === 'O' && check[4].innerText === 'O' && check[2].innerText === 'O') {
-        return alert ('Player O Wins!')
-    } else if (progress.length >= 9) {
-        return alert (`Cat's Game!`)
-    }
+        alert ('Player O Wins!')
+        resetGame ()
+    } else if (board.length >= 9) {
+        alert (`Cat's Game!`)
+        resetGame ()
+    }   
 }
